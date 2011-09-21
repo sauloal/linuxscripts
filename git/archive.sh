f7866094ce9baeb5cc7f6e9c6a45ace2a18bb80b
sauloal <sauloal@gmail.com>
2011-09-21 23:12:10 +0200

    154 1316639530 2011-09-21 23:12:10+02:00

--- git/archive.sh
+++ git/archive.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 git archive HEAD | gzip > archive.tar.gz
 
+#http://gitready.com/intermediate/2009/01/29/exporting-your-repository.html
+#git checkout-index -f -a --prefix=/path/to/folder/
+#git checkout-index -f --prefix=/path/to/folder/ bin/* README.textile
+
--- git/clonebare.sh
+++ git/clonebare.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 git clone --bare linuxscript linuxscript.git
+
+#http://gitready.com/intermediate/2009/01/24/sharing-your-changes.html
+#mkdir test
+#mv jekyll.git test/.git
+#cd test
+#git init
+#  Reinitialized existing Git repository in /Users/qrush/Dev/test/.git/
+# git checkout -f
