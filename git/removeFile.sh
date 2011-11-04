7df4e85a3a2cbe59d6c38cce2e421a068da9717f
saulo <sauloal@gmail.com>
2011-11-04 15:25:27 +0100

    Update git/removeFile.sh

--- git/removeFile.sh
+++ git/removeFile.sh
@@ -6,13 +6,38 @@ FOLDER=$1
 FILE=$2
 #http://help.github.com/removing-sensitive-data/
 
+if [[ ! -d "$FOLDER" ]]; then
+    echo "NO FOLDER DEFINED"
+    exit 1
+fi
+
+if [[ -z "$FILE" ]]; then
+    echo "NO FILE DEFINED"
+    exit 1
+fi
+
+if [[ ! -f "$FOLDER/$FILE" ]]; then
+    echo "NO SUCH FILE '$FOLDER/$FILE'"
+    echo "SHOULD I TRY ANYWAYS? (y/n) "
+    read should
+
+    if [[ "$should" != "y" ]]; then
+        echo "EXITING THEN"
+        exit 1
+    fi
+fi
+
+
 bash commit.sh $FOLDER
-bash clean.sh $FOLDER
+#bash clean.sh $FOLDER
 
 
 SHORTFILE=`basename $FILE`
-cp $FOLDER/$FILE ./$SHORTFILE.bak
+if [[ -f "$FILE" ]]; then
+	cp $FOLDER/$FILE ./$SHORTFILE.bak
+fi
 
+CURRPWD=`pwd`
 cd $FOLDER
 	#git stash
 	CMD="git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch $FILE' HEAD"
@@ -24,7 +49,7 @@ cd $FOLDER
 	git reflog expire --expire=now --all
 	git gc --prune=now
 	git gc --aggressive --prune=now
-cd ..
+cd $CURRPWD
 
 echo "TO RESTORE:"
 echo "cp ./$SHORTFILE.bak $FOLDER/$FILE"
